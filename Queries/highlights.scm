ea80d350729ee68b1445c55970aff57cab9db7a4
eszense <25100769+eszense@users.noreply.github.com>
2023-01-15 13:08:28 +0000

    Initial commit

---
+++ Queries/highlights.scm
@@ -0,0 +1,90 @@
+[
+  "(" ")"
+;  "{" "}"
+] @bracket
+
+[
+  ":="
+  "+="
+  "?="
+] @operator
+
+[
+  "subst"
+  "patsubst"
+  "strip"
+  "findstring"
+  "filter"
+  "filter-out"
+  "sort"
+  "word"
+  "words"
+  "wordlist"
+  "firstword"
+  "lastword"
+  "dir"
+  "notdir"
+  "suffix"
+  "basename"
+  "addsuffix"
+  "addprefix"
+  "join"
+  "wildcard"
+  "realpath"
+  "abspath"
+  "error"
+  "warning"
+  "info"
+  "origin"
+  "flavor"
+  "foreach"
+  "if"
+  "or"
+  "and"
+  "call"
+  "eval"
+  "file"
+  "value"
+] @identifier.function
+
+"shell" @identifier.function
+
+[
+  "ifeq"
+  "ifneq"
+  "else"
+  "endif"
+] @keyword.condition
+
+[ 
+  "override"
+] @keyword.modifier
+
+[
+  "VPATH"
+  ".RECIPEPREFIX"
+] @identifier.core
+
+[
+  (variable_reference)
+  (automatic_variable)
+  (substitution_reference)
+] @identifier.variable
+
+(string) @string
+(text) @string
+
+(comment) @comment
+
+(variable_assignment
+  name: (word) @identifier.variable)
+
+
+(targets) @identifier.function
+(rule
+  target: (_) @string-template) 
+
+(recipe_line
+  ["@" "-" "+"] @keyword.modifier) 
+  
+(prerequisites) @identifier.function
